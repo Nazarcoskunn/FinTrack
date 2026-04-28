e30c663dbe6c5c3b787b986c6c7af9535c3628d7
Nazarcoskunn <coskunnazar73@gmail.com>
2026-04-28 12:07:55 +0300

    Add stock and stockprice domain entities

--- FinTrack.Domain/Entities/Stock.cs
+++ FinTrack.Domain/Entities/Stock.cs
@@ -1,12 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FinTrack.Domain.Entities
 {
     public class Stock
     {
-        
+        public int Id { get; set; }
+
+        public string Symbol { get; set; } = null!; // AAPL
+
+        public string Name { get; set; } = null!; // Apple Inc.
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public List<StockPrice> Prices { get; set; } = new();
     }
 }
--- FinTrack.Domain/Entities/StockPrice.cs
+++ FinTrack.Domain/Entities/StockPrice.cs
@@ -1,12 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FinTrack.Domain.Entities
 {
     public class StockPrice
     {
-        
+        public int Id { get; set; }
+
+        public int StockId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime PriceDate { get; set; } = DateTime.UtcNow;
+
+        public Stock Stock { get; set; } = null!;
     }
 }
