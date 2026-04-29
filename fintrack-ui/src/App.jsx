441c2c17a545427912ea385a007d6e11639bce89
Nazarcoskunn <coskunnazar73@gmail.com>
2026-04-29 23:51:24 +0300

    updated and edited app.jsx

--- fintrack-ui/src/App.jsx
+++ fintrack-ui/src/App.jsx
@@ -5,39 +5,44 @@ import DashboardStats from "./components/DashboardStats";
 import AveragePrices from "./components/AveragePrices";
 import "./App.css";
 
-
+// Main application component
+// Here I combine all relationships and merge components
 
 function App() {
   const [refresh, setRefresh] = useState(false);
 
+  // Triggered when new stock is added
+  // Re-rendering the list by changing the state
   const handleStockAdded = () => {
     setRefresh(!refresh);
   };
 
-return (
-  <div className="app">
-    <div className="header">
-      <h1>FinTrack</h1>
-      <p>Financial data tracking and analysis dashboard</p>
-    </div>
+  return (
+    <div className="app">
+      <div className="header">
+        <h1>📊 FinTrack</h1>
+        <br />
+        <p>Financial data tracking and analysis dashboard</p>
+      </div>
 
-    <div className="card">
-      <StockForm onStockAdded={handleStockAdded} />
+      {/* Stock addition form */}
+      <div className="card">
+        <StockForm onStockAdded={handleStockAdded} />
+      </div>
+      {/* Stock lists */}
+      <div className="card">
+        <StockList key={refresh} />
+      </div>
+      {/* Area showing average prices */}
+      <div className="card">
+        <AveragePrices refresh={refresh} />
+      </div>
+      {/* Dashboard statistics */}
+      <div className="card">
+        <DashboardStats />
+      </div>
     </div>
-
-    <div className="card">
-      <StockList key={refresh} />
-    </div>
-
-    <div className="card">
-   <AveragePrices refresh={refresh} /> 
-    </div>
-
-    <div className="card">
-  <DashboardStats />
-</div>
-  </div>
-);
+  );
 }
 
 export default App;
