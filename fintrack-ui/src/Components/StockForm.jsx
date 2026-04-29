f1d23c14232cede81645302bf990ae50f26129f9
Nazarcoskunn <coskunnazar73@gmail.com>
2026-04-29 16:45:37 +0300

    added stock form component for creating new stocks

---
+++ fintrack-ui/src/Components/StockForm.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import api from "../api";
+
+function StockForm({ onStockAdded }) {
+  const [symbol, setSymbol] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await api.post("/stocks", {
+        symbol,
+        name,
+      });
+
+      setSymbol("");
+      setName("");
+      onStockAdded();
+    } catch (err) {
+      console.error(err);
+      alert("Stock eklenirken hata oluştu");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Stock Ekle</h3>
+
+      <input
+        type="text"
+        placeholder="Symbol (AAPL)"
+        value={symbol}
+        onChange={(e) => setSymbol(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Name (Apple)"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <button type="submit">Ekle</button>
+    </form>
+  );
+}
+
+export default StockForm;
