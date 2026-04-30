6437ea12110f7a7cc0d18978f131dec87631af0f
Nazarcoskunn <coskunnazar73@gmail.com>
2026-04-30 10:47:52 +0300

    added bar chart visualization to dashboard stats

--- fintrack-ui/src/components/DashboardStats.jsx
+++ fintrack-ui/src/components/DashboardStats.jsx
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import api from "../api";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 // This component shows simple dashboard stats
 // I calculate highest and lowest average prices to give quick insight
-
+// This component shows simple dashboard stats
+// I calculate highest and lowest average prices to give quick insight
 
 function DashboardStats() {
   const [averages, setAverages] = useState([]);
@@ -30,17 +39,39 @@ function DashboardStats() {
     : null;
 
   return (
-    <div className="stats-grid">
-      <div className="stat-card">
-       <span>📈 Highest Average</span>
-        <h3>{highest ? highest.symbol : "-"}</h3>
-        <p>{highest ? `$${highest.averagePrice}` : "-"}</p>
+    <div>
+      <div className="stats-grid">
+        <div className="stat-card">
+          <span>📈 Highest Average</span>
+          <h3>{highest ? highest.symbol : "-"}</h3>
+          <p>{highest ? `$${highest.averagePrice}` : "-"}</p>
+        </div>
+
+        <div className="stat-card">
+          <span>📉 Lowest Average</span>
+          <h3>{lowest ? lowest.symbol : "-"}</h3>
+          <p>{lowest ? `$${lowest.averagePrice}` : "-"}</p>
+        </div>
       </div>
 
-      <div className="stat-card">
-        <span>📉 Lowest Average</span>
-        <h3>{lowest ? lowest.symbol : "-"}</h3>
-        <p>{lowest ? `$${lowest.averagePrice}` : "-"}</p>
+      <div className="chart-card">
+        <h3>Average Price Chart</h3>
+
+        {averages.length === 0 ? (
+          <p>No average price data found.</p>
+        ) : (
+          // responsive chart for better UI
+          <ResponsiveContainer width="100%" height={260}>
+            <BarChart data={averages}>
+                {/* X axis shows stock symbols */}
+              <XAxis dataKey="symbol" />
+               {/* Y axis shows price values */}
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="averagePrice" fill="#3b82f6" />
+            </BarChart>
+          </ResponsiveContainer>
+        )}
       </div>
     </div>
   );
